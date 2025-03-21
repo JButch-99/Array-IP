9f104d8de11e7afeda70c4800c3261a63994dd77
Josh Butcher <sjwb1999@gmail.com>
2025-03-21 14:48:26 -0700

    make updates to text files and JS bodies, in addition to adding the processArray.txt file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
 // Business Logic
 
-function processNumber(value) {
-  if (value <= 0 || typeof value !== "number") {
+function validNumberToArray(value) {
+  if (value <= 0 || typeof value !== "number" || value === '') {
     return null;
+  } 
+
+  let valueString = value.toString().split('');
+
+  return valueString;
+}
+
+function processArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (parseInt(array[i]) === 3) {
+      array[i] = "Won't you be my neighbor?"
+    } else {
+      array[i] = "";
+    }
   }
-  console.log(value);
+
+  console.log(array);
 }
 
 // UI Logic
 function handleSubmission() {
-  const numberString = -3;
-  processNumber(numberString);
+  const numberString = 13054;
+  const numArray = validNumberToArray(numberString);
+  processArray(numArray);
 }
 
 window.addEventListener("load", function(){

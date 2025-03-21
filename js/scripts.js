f06b554aad59bf0360002f2102a5f6614ae9c2a1
Josh Butcher <sjwb1999@gmail.com>
2025-03-21 16:00:33 -0700

    update JS and processArray.txt files.

--- js/scripts.js
+++ js/scripts.js
@@ -11,22 +11,34 @@ function validNumberToArray(value) {
 }
 
 function processArray(array) {
+  let newArray = [];
+
   for (let i = 0; i < array.length; i++) {
-    if (parseInt(array[i]) === 3) {
-      array[i] = "Won't you be my neighbor?"
-    } else {
-      array[i] = "";
+    if (parseInt(array[i]) === 1) {
+      array[i] = "Beep!";
+      newArray.push(array[i]);
+    } else if (parseInt(array[i]) === 2) {
+      array[i] = "Boop!";
+      newArray.push(array[i]);
+    } else if (parseInt(array[i]) === 3) {
+      array[i] = "Won't you be my neighbor?";
+      newArray.push(array[i]);
     }
   }
 
-  console.log(array);
+  console.log(newArray);
+  return newArray;
 }
 
 // UI Logic
 function handleSubmission() {
-  const numberString = 13054;
+  const numberString = 13254;
+
+  // Business Logic
   const numArray = validNumberToArray(numberString);
   processArray(numArray);
+
+  // UI Logic
 }
 
 window.addEventListener("load", function(){
